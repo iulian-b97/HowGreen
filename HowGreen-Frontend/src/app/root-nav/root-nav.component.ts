1a75ea9582f93fbd7fa4b993ae5e6d4467359e96
unknown <iulian.burghelea1997@gmail.com>
2021-05-06 20:38:05 +0300

    Add endpoints

--- HowGreen-Frontend/src/app/root-nav/root-nav.component.ts
+++ HowGreen-Frontend/src/app/root-nav/root-nav.component.ts
@@ -1,18 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, shareReplay } from 'rxjs/operators';
+import { Router } from '@angular/router';
 declare const showMenu: any;
 declare const hideMenu: any;
 
 @Component({
   selector: 'app-root-nav',
   templateUrl: './root-nav.component.html',
-  styleUrls: ['./root-nav.component.css']
+  styleUrls: ['./root-nav.component.css'],
+  providers: [
+    UserService
+  ]
 })
 export class RootNavComponent implements OnInit {
 
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver, private service: UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  isLog(): boolean
+  {
+    return this.service.isLogged();
+  }
+
+  onLogout()
+  {
+    this.service.logout();
+    return this.router.navigate(['/user/login']);
+  }
+
 
   show() {
     showMenu();
--- HowGreen-Frontend/src/app/user/login/login.component.ts
+++ HowGreen-Frontend/src/app/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  formModel={
+    UserName : '',
+    Password : ''
   }
 
+  constructor(private service:UserService, private router:Router, private toastr:ToastrService) { }
+
+  ngOnInit(): void {
+    if(localStorage.getItem('token') != null )
+      this.router.navigateByUrl('/home');
+  }
+
+  onSubmit(form:NgForm){
+    this.service.login(form.value).subscribe(
+      (res:any)=>{
+        localStorage.setItem('token',res.token);
+        this.router.navigateByUrl('/home');
+        localStorage.setItem('loggedIn', 'true');
+        this.service.isLoggedIn$.next(true);
+      },
+      err=>{
+        if(err.status == 400)
+        {
+          this.toastr.error('Nume de utilizator sau parola incorecta.','Autentificare esuata.');
+          localStorage.setItem('loggedIn','false');
+          this.service.isLoggedIn$.next(false);
+        }
+        else
+          console.log(err);
+      }
+    )
+  }
 }
